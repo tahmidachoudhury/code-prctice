6b6884e18e08bc4d57a2ad693f263b8c7e40625b
tahmid choudhury <tahmidachoudhury@outlook.com>
2024-01-17 15:47:12 +0000

    testing dictionaries using shopping list context

---
+++ dict-practice.py
@@ -0,0 +1,9 @@
+shoppingList = {
+    "butcher": ["Chicken legs", "Chicken wings"],
+    "food store": ["Cabbage", "Rice", "Bananas", "Oranges"],
+    "Other": 'Spices'
+}
+
+print(shoppingList["butcher"][1])
+
+print(shoppingList["Other"])
