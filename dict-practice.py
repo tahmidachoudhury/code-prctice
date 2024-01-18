5040a74e93905543b9b57ece8c3fed851a658915
tahmid choudhury <tahmidachoudhury@outlook.com>
2024-01-18 10:09:58 +0000

    more dict practice

--- dict-practice.py
+++ dict-practice.py
@@ -4,10 +4,25 @@ shoppingList = {
     "Other": 'Spices'
 }
 
-print(shoppingList["butcher"][1])
+#print(shoppingList["butcher"][1])
 
-print(shoppingList["Other"])
+#print(shoppingList["Other"])
 
 shoppingList["snacks"] = ['Chocolate', 'Walkers Crisps']
 
-print(shoppingList)
+#print(shoppingList)
+
+practice_dict = { 
+    "London": "England", 
+    "Edinburgh": "Scotland", 
+    "Cardiff": "Wales", 
+    "Belfast": "Northern Ireland" 
+}
+
+#print(practice_dict.keys())
+#print(practice_dict.values())
+#print(practice_dict.get('London'))
+#print(practice_dict.items())
+#print(practice_dict.pop('Belfast'))
+#print(practice_dict.clear())
+#print(practice_dict.setdefault('Edinburgh', 'Manchester'))
